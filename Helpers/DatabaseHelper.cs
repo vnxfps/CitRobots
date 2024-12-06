2fc92caebcc7621051b26c90cd3eb31b754d6302
Noah <noahdailha@gmail.com>
2024-12-06 03:25:51 -0300

    Att2

--- Helpers/DatabaseHelper.cs
+++ Helpers/DatabaseHelper.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace CitRobots.Helpers
 {
     public class DatabaseHelper
     {
+        // Configure sua string de conexão aqui
+        private static readonly string ConnectionString = "Server=localhost;Database=CitRobots;Uid=root;Pwd=;";
 
-        private static string ConnectionString = "Server=localhost;Database=CitRobots;Uid=root;Pwd=;";
-
+        /// <summary>
+        /// Testa a conexão com o banco de dados.
+        /// </summary>
         public void TestConnection()
         {
             try
             {
-                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                using (var connection = new MySqlConnection(ConnectionString))
                 {
                     connection.Open();
                     Console.WriteLine("Conexão com banco de dados estabelecida com sucesso!");
@@ -29,83 +29,108 @@ namespace CitRobots.Helpers
             }
         }
 
+        /// <summary>
+        /// Executa um comando SELECT e retorna os resultados como um DataTable.
+        /// </summary>
         public DataTable ExecuteSelect(string query, Dictionary<string, object> parameters = null)
         {
-            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+            try
             {
-                connection.Open();
-                using (MySqlCommand command = new MySqlCommand(query, connection))
+                using (var connection = new MySqlConnection(ConnectionString))
                 {
-                    if (parameters != null)
+                    connection.Open();
+                    using (var command = new MySqlCommand(query, connection))
                     {
-                        foreach (var param in parameters)
-                        {
-                            command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
-                        }
-                    }
+                        AddParameters(command, parameters);
 
-                    DataTable dt = new DataTable();
-                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
-                    {
-                        adapter.Fill(dt);
+                        var dt = new DataTable();
+                        using (var adapter = new MySqlDataAdapter(command))
+                        {
+                            adapter.Fill(dt);
+                        }
+                        return dt;
                     }
-                    return dt;
                 }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao executar SELECT: {ex.Message}");
             }
         }
 
+        /// <summary>
+        /// Executa um comando INSERT e retorna o ID do último registro inserido.
+        /// </summary>
         public int ExecuteInsert(string query, Dictionary<string, object> parameters)
         {
-            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+            try
             {
-                connection.Open();
-                using (MySqlCommand command = new MySqlCommand(query, connection))
+                using (var connection = new MySqlConnection(ConnectionString))
                 {
-                    foreach (var param in parameters)
+                    connection.Open();
+                    using (var command = new MySqlCommand(query + "; SELECT LAST_INSERT_ID();", connection))
                     {
-                        command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+                        AddParameters(command, parameters);
+                        return Convert.ToInt32(command.ExecuteScalar());
                     }
-
-                    query += "; SELECT LAST_INSERT_ID();";
-                    command.CommandText = query;
-
-                    return Convert.ToInt32(command.ExecuteScalar());
                 }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao executar INSERT: {ex.Message}");
             }
         }
 
+        /// <summary>
+        /// Executa um comando UPDATE e retorna o número de linhas afetadas.
+        /// </summary>
         public int ExecuteUpdate(string query, Dictionary<string, object> parameters)
         {
-            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
-            {
-                connection.Open();
-                using (MySqlCommand command = new MySqlCommand(query, connection))
-                {
-                    foreach (var param in parameters)
-                    {
-                        command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
-                    }
-
-                    return command.ExecuteNonQuery();
-                }
-            }
+            return ExecuteNonQuery(query, parameters, "UPDATE");
         }
 
+        /// <summary>
+        /// Executa um comando DELETE e retorna o número de linhas afetadas.
+        /// </summary>
         public int ExecuteDelete(string query, Dictionary<string, object> parameters)
         {
-            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
-            {
-                connection.Open();
-                using (MySqlCommand command = new MySqlCommand(query, connection))
-                {
-                    foreach (var param in parameters)
-                    {
-                        command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
-                    }
+            return ExecuteNonQuery(query, parameters, "DELETE");
+        }
 
-                    return command.ExecuteNonQuery();
+        /// <summary>
+        /// Método genérico para executar comandos como INSERT, UPDATE e DELETE.
+        /// </summary>
+        private int ExecuteNonQuery(string query, Dictionary<string, object> parameters, string commandType)
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    using (var command = new MySqlCommand(query, connection))
+                    {
+                        AddParameters(command, parameters);
+                        return command.ExecuteNonQuery();
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao executar {commandType}: {ex.Message}");
+            }
         }
-    }   
+
+        /// <summary>
+        /// Adiciona parâmetros ao comando MySQL.
+        /// </summary>
+        private void AddParameters(MySqlCommand command, Dictionary<string, object> parameters)
+        {
+            if (parameters == null) return;
+
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+            }
+        }
+    }
 }
--- Views/ProfileForm.cs
+++ Views/ProfileForm.cs
@@ -1,220 +1,125 @@
 ﻿using System;
-using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
-using CitRobots.Models;
-using CitRobots.Services;
 
 namespace CitRobots.Views
 {
     public partial class ProfileForm : Form
     {
-        private UserModel currentUser;
-        private TabControl tabControl;
-        private Panel profilePanel;
+        private Panel menuPanel;
+        private Panel contentPanel;
+        private Label lblTitle;
 
         public ProfileForm()
         {
             InitializeComponent();
             SetupProfileForm();
-            LoadUserData();
-            CenterToScreen();
         }
 
         private void SetupProfileForm()
         {
-            this.BackColor = Color.FromArgb(45, 45, 45);
-            this.Size = new Size(800, 600);
+            this.Text = "Perfil do Usuário";
+            this.BackColor = Color.FromArgb(35, 35, 35);
+            this.Size = new Size(850, 600);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
             this.MaximizeBox = false;
 
-            Panel mainContainer = new Panel
+            menuPanel = new Panel
+            {
+                Dock = DockStyle.Left,
+                Width = 200,
+                BackColor = Color.FromArgb(45, 45, 45),
+                Padding = new Padding(10)
+            };
+
+            contentPanel = new Panel
             {
                 Dock = DockStyle.Fill,
                 BackColor = Color.FromArgb(45, 45, 45),
-                Padding = new Padding(20),
-                AutoScroll = true
+                Padding = new Padding(20)
             };
 
-            PictureBox logoBox = new PictureBox
-            {
-                Size = new Size(200, 100),
-                SizeMode = PictureBoxSizeMode.Zoom
-            };
-            logoBox.Location = new Point((this.ClientSize.Width - logoBox.Width) / 2, 20);
-
-            try
-            {
-                logoBox.Image = Image.FromFile("Resources/Images/Logo.png");
-            }
-            catch
-            {
-                logoBox.Visible = false;
-            }
-
-            Label lblTitle = new Label
+            lblTitle = new Label
             {
                 Text = "Meu Perfil",
-                Font = new Font("Segoe UI", 32, FontStyle.Bold),
+                Font = new Font("Segoe UI", 20, FontStyle.Bold),
                 ForeColor = Color.White,
-                TextAlign = ContentAlignment.MiddleCenter,
-                Size = new Size(400, 60)
-            };
-            lblTitle.Location = new Point((this.ClientSize.Width - lblTitle.Width) / 2, logoBox.Bottom + 20);
-
-            profilePanel = new Panel
-            {
-                Width = 700,
-                Height = 450,
-                BackColor = Color.FromArgb(35, 35, 35)
-            };
-            profilePanel.Location = new Point((this.ClientSize.Width - profilePanel.Width) / 2, lblTitle.Bottom + 30);
-
-            profilePanel.Paint += (s, e) =>
-            {
-                var rect = new Rectangle(0, 0, profilePanel.Width - 1, profilePanel.Height - 1);
-                using (var pen = new Pen(Color.FromArgb(92, 184, 92), 2))
-                {
-                    e.Graphics.DrawRectangle(pen, rect);
-                }
+                Dock = DockStyle.Top,
+                Height = 50,
+                TextAlign = ContentAlignment.MiddleCenter
             };
 
-            SetupTabs();
+            this.Controls.Add(contentPanel);
+            this.Controls.Add(menuPanel);
+            contentPanel.Controls.Add(lblTitle);
 
-            mainContainer.Controls.AddRange(new Control[] { logoBox, lblTitle, profilePanel });
-            this.Controls.Add(mainContainer);
-
-            this.Resize += (s, e) =>
-            {
-                logoBox.Location = new Point((this.ClientSize.Width - logoBox.Width) / 2, 20);
-                lblTitle.Location = new Point((this.ClientSize.Width - lblTitle.Width) / 2, logoBox.Bottom + 20);
-                profilePanel.Location = new Point((this.ClientSize.Width - profilePanel.Width) / 2, lblTitle.Bottom + 30);
-            };
+            SetupMenu();
+            ShowProfileData(null, null);
         }
 
-        private void SetupTabs()
+        private Button CreateMenuButton(string text, EventHandler onClick)
         {
-            tabControl = new TabControl
+            Button btn = new Button
             {
-                Dock = DockStyle.Fill,
-                Font = new Font("Segoe UI", 12F),
-                Padding = new Point(10, 10)
-            };
-
-            TabPage tabDados = new TabPage("Dados Pessoais")
-            {
-                BackColor = Color.FromArgb(45, 45, 45)
-            };
-
-            TabPage tabPedidos = new TabPage("Meus Pedidos")
-            {
-                BackColor = Color.FromArgb(45, 45, 45)
-            };
-
-            TabPage tabEndereco = new TabPage("Endereços")
-            {
-                BackColor = Color.FromArgb(45, 45, 45)
-            };
-
-            SetupDadosPessoais(tabDados);
-            SetupPedidos(tabPedidos);
-            SetupEnderecos(tabEndereco);
-
-            tabControl.TabPages.AddRange(new TabPage[] { tabDados, tabPedidos, tabEndereco });
-            profilePanel.Controls.Add(tabControl);
-        }
-
-        private void SetupDadosPessoais(TabPage tab)
-        {
-            Panel panel = new Panel
-            {
-                Dock = DockStyle.Fill,
-                Padding = new Padding(20),
-                BackColor = Color.FromArgb(35, 35, 35)
-            };
-
-            int yPos = 20;
-            AddProfileField(panel, "Nome:", ref yPos);
-            AddProfileField(panel, "Sobrenome:", ref yPos);
-            AddProfileField(panel, "Email:", ref yPos);
-            AddProfileField(panel, "CPF:", ref yPos);
-            AddProfileField(panel, "Telefone:", ref yPos);
-
-            Button btnSalvar = new Button
-            {
-                Text = "Salvar Alterações",
-                Size = new Size(360, 45),
-                Location = new Point(20, yPos + 15),
-                BackColor = Color.FromArgb(92, 184, 92),
+                Text = text,
+                Dock = DockStyle.Top,
+                Height = 50,
+                Font = new Font("Segoe UI", 12),
+                BackColor = Color.FromArgb(45, 45, 45),
                 ForeColor = Color.White,
                 FlatStyle = FlatStyle.Flat,
-                Font = new Font("Segoe UI", 14, FontStyle.Bold),
                 Cursor = Cursors.Hand
             };
-            btnSalvar.FlatAppearance.BorderSize = 0;
-            btnSalvar.Click += BtnSalvar_Click;
-
-            panel.Controls.Add(btnSalvar);
-            tab.Controls.Add(panel);
+            btn.FlatAppearance.BorderSize = 0;
+            btn.Click += onClick;
+            return btn;
         }
 
-        private void AddProfileField(Panel panel, string labelText, ref int yPos)
+        private void SetupMenu()
         {
-            Label lbl = new Label
+            Label lblMenu = new Label
             {
-                Text = labelText,
-                Location = new Point(20, yPos),
-                Size = new Size(200, 25),
-                Font = new Font("Segoe UI", 12),
-                ForeColor = Color.White
-            };
-
-            TextBox txt = new TextBox
-            {
-                Location = new Point(20, yPos + 25),
-                Size = new Size(360, 35),
-                Font = new Font("Segoe UI", 12),
-                BackColor = Color.FromArgb(45, 45, 45),
+                Text = "Menu",
+                Font = new Font("Segoe UI", 14, FontStyle.Bold),
                 ForeColor = Color.White,
-                BorderStyle = BorderStyle.FixedSingle
+                Dock = DockStyle.Top,
+                Height = 40,
+                TextAlign = ContentAlignment.MiddleCenter
             };
+            menuPanel.Controls.Add(lblMenu);
 
-            panel.Controls.AddRange(new Control[] { lbl, txt });
-            yPos += 65;
+            menuPanel.Controls.Add(CreateMenuButton("🛒 Meus Pedidos", ShowOrders));
+            menuPanel.Controls.Add(CreateMenuButton("📍 Alterar Endereço", ShowAddress));
+            menuPanel.Controls.Add(CreateMenuButton("👤 Alterar Informações", ShowProfileData));
         }
 
-        private void SetupPedidos(TabPage tab)
+        private void ShowProfileData(object sender, EventArgs e)
         {
-            ListView listPedidos = new ListView
-            {
-                Dock = DockStyle.Fill,
-                View = View.Details,
-                FullRowSelect = true,
-                GridLines = true,
-                Font = new Font("Segoe UI", 10F),
-                BackColor = Color.FromArgb(45, 45, 45),
-                ForeColor = Color.White
-            };
+            contentPanel.Controls.Clear();
+            lblTitle.Text = "👤 Alterar Informações do Perfil";
 
-            listPedidos.Columns.AddRange(new ColumnHeader[]
-            {
-                new ColumnHeader { Text = "Número", Width = 100 },
-                new ColumnHeader { Text = "Data", Width = 100 },
-                new ColumnHeader { Text = "Status", Width = 100 },
-                new ColumnHeader { Text = "Total", Width = 100 }
-            });
+            Panel profilePanel = CreateContentPanel();
 
-            tab.Controls.Add(listPedidos);
+            int yPos = 20;
+            AddProfileField(profilePanel, "Nome:", ref yPos);
+            AddProfileField(profilePanel, "Sobrenome:", ref yPos);
+            AddProfileField(profilePanel, "Email:", ref yPos);
+            AddProfileField(profilePanel, "CPF:", ref yPos);
+            AddProfileField(profilePanel, "Telefone:", ref yPos);
+
+            Button btnSave = CreateActionButton("Salvar Alterações", "💾", yPos + 20);
+            profilePanel.Controls.Add(btnSave);
+
+            contentPanel.Controls.Add(profilePanel);
         }
 
-        private void SetupEnderecos(TabPage tab)
+        private void ShowAddress(object sender, EventArgs e)
         {
-            Panel panel = new Panel
-            {
-                Dock = DockStyle.Fill,
-                Padding = new Padding(20),
-                BackColor = Color.FromArgb(35, 35, 35)
-            };
+            contentPanel.Controls.Clear();
+            lblTitle.Text = "📍 Alterar Endereço Principal";
+
+            Panel addressPanel = CreateContentPanel();
 
             Label lblEndereco = new Label
             {
@@ -228,7 +133,7 @@ namespace CitRobots.Views
             TextBox txtEndereco = new TextBox
             {
                 Location = new Point(20, 50),
-                Size = new Size(360, 100),
+                Size = new Size(400, 100),
                 Font = new Font("Segoe UI", 12),
                 Multiline = true,
                 BackColor = Color.FromArgb(45, 45, 45),
@@ -236,31 +141,107 @@ namespace CitRobots.Views
                 BorderStyle = BorderStyle.FixedSingle
             };
 
-            Button btnSalvarEndereco = new Button
+            Button btnSaveAddress = CreateActionButton("Salvar Endereço", "📥", 170);
+            addressPanel.Controls.Add(lblEndereco);
+            addressPanel.Controls.Add(txtEndereco);
+            addressPanel.Controls.Add(btnSaveAddress);
+
+            contentPanel.Controls.Add(addressPanel);
+        }
+
+        private void ShowOrders(object sender, EventArgs e)
+        {
+            contentPanel.Controls.Clear();
+            lblTitle.Text = "🛒 Meus Pedidos";
+
+            Panel ordersPanel = CreateContentPanel();
+
+            Label lblPedidos = new Label
             {
-                Text = "Salvar Endereço",
-                Size = new Size(360, 45),
-                Location = new Point(20, 170),
+                Text = "Seus Pedidos Recentes:",
+                Font = new Font("Segoe UI", 14, FontStyle.Bold),
+                ForeColor = Color.White,
+                Dock = DockStyle.Top,
+                Height = 30
+            };
+
+            ordersPanel.Controls.Add(lblPedidos);
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Label lblOrder = new Label
+                {
+                    Text = $"Pedido #{i} - {DateTime.Now.AddDays(-i):dd/MM/yyyy} - Status: Enviado - Total: R$ {100 + i * 10}",
+                    Font = new Font("Segoe UI", 12),
+                    ForeColor = Color.White,
+                    Dock = DockStyle.Top,
+                    Padding = new Padding(5),
+                    BackColor = Color.FromArgb(50, 50, 50)
+                };
+                ordersPanel.Controls.Add(lblOrder);
+            }
+
+            contentPanel.Controls.Add(ordersPanel);
+        }
+
+        private Panel CreateContentPanel()
+        {
+            return new Panel
+            {
+                Dock = DockStyle.Fill,
+                Padding = new Padding(20),
+                BackColor = Color.FromArgb(35, 35, 35),
+                AutoScroll = true
+            };
+        }
+
+        private Button CreateActionButton(string text, string emoji, int yPos)
+        {
+            return new Button
+            {
+                Text = $"{emoji} {text}",
+                Size = new Size(200, 40),
+                Location = new Point((contentPanel.Width - 200) / 2, yPos),
                 BackColor = Color.FromArgb(92, 184, 92),
                 ForeColor = Color.White,
                 FlatStyle = FlatStyle.Flat,
-                Font = new Font("Segoe UI", 14, FontStyle.Bold),
                 Cursor = Cursors.Hand
+            }.Apply(b => b.FlatAppearance.BorderSize = 0);
+        }
+
+        private void AddProfileField(Panel panel, string labelText, ref int yPos)
+        {
+            Label lbl = new Label
+            {
+                Text = labelText,
+                Location = new Point(20, yPos),
+                Size = new Size(150, 25),
+                Font = new Font("Segoe UI", 12),
+                ForeColor = Color.White
             };
-            btnSalvarEndereco.FlatAppearance.BorderSize = 0;
 
-            panel.Controls.AddRange(new Control[] { lblEndereco, txtEndereco, btnSalvarEndereco });
-            tab.Controls.Add(panel);
+            TextBox txt = new TextBox
+            {
+                Location = new Point(20, yPos + 30),
+                Size = new Size(400, 30),
+                Font = new Font("Segoe UI", 12),
+                BackColor = Color.FromArgb(45, 45, 45),
+                ForeColor = Color.White,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            panel.Controls.Add(lbl);
+            panel.Controls.Add(txt);
+            yPos += 80;
         }
+    }
 
-        private void BtnSalvar_Click(object sender, EventArgs e)
+    public static class Extensions
+    {
+        public static T Apply<T>(this T target, Action<T> action)
         {
-            // Implementação para salvar alterações.
-        }
-
-        private void LoadUserData()
-        {
-            // Carregar dados do usuário atual.
+            action(target);
+            return target;
         }
     }
 }
